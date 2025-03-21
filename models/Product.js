c316a3ad253a80d3af07f91358bc32c15ff314ed
SmartMavuno <info@smartmavuno.com>
2025-03-21 07:34:07 +0300

    Backedn Done

--- models/Product.js
+++ models/Product.js
@@ -11,8 +11,56 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      enum: ["Toyota", "Honda", "Nissan", "Mazda", "Subaru", "Mitsubishi"],
-      set: (value) => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase(), // Fix case sensitivity
+      enum: [
+        "Toyota",
+        "Honda",
+        "Nissan",
+        "Mazda",
+        "Subaru",
+        "Mitsubishi",
+        "Suzuki",
+        "Isuzu",
+
+        "Mercedes-Benz",
+        "BMW",
+        "Audi",
+        "Volkswagen",
+        "Porsche",
+        "Opel",
+
+        "Ford",
+        "Chevrolet",
+        "Dodge",
+        "Jeep",
+        "Tesla",
+        "Cadillac",
+
+        "Hyundai",
+        "Kia",
+        "Genesis",
+
+        "Land Rover",
+        "Jaguar",
+        "Rolls-Royce",
+        "Bentley",
+        "Mini",
+
+        "Ferrari",
+        "Lamborghini",
+        "Maserati",
+        "Alfa Romeo",
+        "Fiat",
+
+        "Peugeot",
+        "Renault",
+        "Citroën",
+        "BYD",
+        "Geely",
+        "Chery",
+        "Haval",
+      ],
+      set: (value) =>
+        value.charAt(0).toUpperCase() + value.slice(1).toLowerCase(), // Fix case sensitivity
     },
     model: {
       type: String,
